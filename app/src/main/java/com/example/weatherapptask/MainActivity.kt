e21885471f79734384e1fe54435af8522fdfe033
f.r.malikov <fuad.malikov@rabitabank.com>
2021-12-17 16:45:58 +0400

    Weather app.

--- app/src/main/java/com/example/weatherapptask/MainActivity.kt
+++ app/src/main/java/com/example/weatherapptask/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.weatherapptask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.weatherapptask.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val bottomNav: BottomNavigationView = binding.bottomNav
+        val navController = findNavController(R.id.fragment)
+        bottomNav.setupWithNavController(navController)
+        bottomNav.itemIconTintList = null
     }
 }
