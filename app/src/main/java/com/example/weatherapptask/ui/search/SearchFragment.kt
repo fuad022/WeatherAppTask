df1356074b6a8d3d3ef568acc2c4b1684635ff9e
f.r.malikov <fuad.malikov@rabitabank.com>
2021-12-22 11:15:05 +0400

    Weather app.

--- app/src/main/java/com/example/weatherapptask/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/weatherapptask/ui/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -17,6 +18,8 @@ import com.example.weatherapptask.data.remote.other.Constants.Companion.API_KEY
 import com.example.weatherapptask.data.remote.other.Constants.Companion.BAKU_CITY
 import com.example.weatherapptask.data.remote.other.Constants.Companion.UNITS
 import com.example.weatherapptask.databinding.FragmentSearchBinding
+import org.json.JSONArray
+import org.json.JSONObject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
@@ -76,9 +79,24 @@ class SearchFragment : Fragment() {
             if (response.isSuccessful) {
                 response.body()?.let {
                     binding.temp.text = it.temperatureInfo.temp.toString()
+                    binding.weather.text = it.weather[0].currentWeather
                     binding.city.text = it.cityName
                 }
             }
         })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
