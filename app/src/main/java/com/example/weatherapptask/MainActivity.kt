08c229f86723ce326b2e2e609d1c7dfad3e8ead4
f.r.malikov <fuad.malikov@rabitabank.com>
2021-12-24 16:52:41 +0400

    Weather app.

--- app/src/main/java/com/example/weatherapptask/MainActivity.kt
+++ app/src/main/java/com/example/weatherapptask/MainActivity.kt
@@ -2,21 +2,44 @@ package com.example.weatherapptask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.weatherapptask.databinding.ActivityMainBinding
+import com.example.weatherapptask.ui.mylocation.MyLocationFragment
+import com.example.weatherapptask.ui.search.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        
+
+//        val bottomNav: BottomNavigationView = binding.bottomNav
+//        val navController = findNavController(R.id.fragment)
+//        bottomNav.setupWithNavController(navController)
+//        bottomNav.itemIconTintList = null
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, MyLocationFragment()).commit()
         val bottomNav: BottomNavigationView = binding.bottomNav
-        val navController = findNavController(R.id.fragment)
-        bottomNav.setupWithNavController(navController)
         bottomNav.itemIconTintList = null
+
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+    }
+
+    val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        when (it.itemId) {
+            R.id.location -> {
+                currentFragment = MyLocationFragment()
+            }
+            R.id.search -> {
+                currentFragment = SearchFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, currentFragment).commit()
+        true
     }
 }
