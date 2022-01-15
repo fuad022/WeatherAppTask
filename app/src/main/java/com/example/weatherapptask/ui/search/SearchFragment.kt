1a8428e9ff1ea6096fb3c9d5debae4dfb9c3f3f7
Fuad <fuad-022@mail.ru>
2022-01-15 14:06:23 +0400

    Some changes.

--- app/src/main/java/com/example/weatherapptask/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/weatherapptask/ui/search/SearchFragment.kt
@@ -4,7 +4,10 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,7 +18,9 @@ import androidx.core.app.ActivityCompat
 import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import com.example.weatherapptask.data.remote.LocationForecastVM
+import com.example.weatherapptask.data.remote.other.Constants
 import com.example.weatherapptask.data.remote.other.Constants.Companion.API_KEY
+import com.example.weatherapptask.data.remote.other.Constants.Companion.PERMISSION_ID
 import com.example.weatherapptask.data.remote.other.Constants.Companion.UNITS
 import com.example.weatherapptask.data.remote.other.NetworkResult
 import com.example.weatherapptask.databinding.FragmentSearchBinding
@@ -23,8 +28,7 @@ import com.example.weatherapptask.ui.search.viewmodel.SearchForecastVM
 import com.example.weatherapptask.util.Util.displayToast
 import com.example.weatherapptask.util.Util.getWeatherAnimation
 import com.example.weatherapptask.util.Util.getWholeNum
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
@@ -32,6 +36,7 @@ class SearchFragment : Fragment() {
     private val locationForecastVM: LocationForecastVM by viewModel()
     private val searchForecastVM: SearchForecastVM by viewModel()
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
     var toast: Toast? = null
 
     override fun onCreateView(
@@ -43,39 +48,6 @@ class SearchFragment : Fragment() {
         initBtn()
         return binding.root
     }
-
-    /*
-    @SuppressLint("ClickableViewAccessibility")
-    fun EditText.setupClearButtonWithAction() {
-        addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(ed: Editable?) {
-                getCurrentCityForecast(ed.toString())
-                val clearIcon = if (ed?.isNotEmpty() == true) R.drawable.ic_clear else 0
-                setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search_on, 0, clearIcon, 0)
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
-        })
-
-        setOnTouchListener(View.OnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                if (event.rawX >= (this.right - this.compoundPaddingRight)) {
-                    this.setText("")
-                    binding.card.isVisible = false
-                    return@OnTouchListener true
-                }
-            }
-            return@OnTouchListener false
-        })
-    }
-
-    private fun initBtn() {
-        binding.btn.setOnClickListener {
-            binding.search.setText("")
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-            fetchLocation()
-        }
-    }*/
 
     private fun clickSearchBtn() {
         binding.searchBtn.setOnClickListener {
@@ -98,25 +70,82 @@ class SearchFragment : Fragment() {
         binding.btn.setOnClickListener {
             binding.searchText.setText("")
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-            fetchLocation()
+//            fetchLocation()
+            getLastLocation()
             binding.btn.isClickable = false
         }
     }
 
-    private fun fetchLocation() {
-        val task = fusedLocationProviderClient.lastLocation
+    ///////////////////////////////New check permission
 
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED && ActivityCompat
-                .checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
-            return
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true
         }
-        task.addOnSuccessListener {
-            getCurrentCityForecastByCoordinates(it.latitude.toString(),it.longitude.toString())
+        return false
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = context!!.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug", "You have the permission")
+            }
         }
     }
+
+    private fun getLastLocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    var location = task.result
+                    if (location == null) {
+                        getNewLocation()
+                    } else {
+                        getCurrentCityForecastByCoordinates(location.latitude.toString(),location.longitude.toString())
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Please, enable your location service", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            requestPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getNewLocation() {
+        locationRequest = LocationRequest.create().apply {
+            interval = 0
+            fastestInterval = 0
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            numUpdates = 2
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()!!
+        )
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLocation = p0.lastLocation
+            getCurrentCityForecastByCoordinates(lastLocation.latitude.toString(), lastLocation.longitude.toString())
+        }
+    }
+
+    ///////////////////////////////New check permission
 
     private fun getCurrentCityForecastByCoordinates(lat: String, lon: String) {
 //        locationForecastVM.sendData(lat, lon, UNITS, API_KEY)
@@ -207,6 +236,56 @@ class SearchFragment : Fragment() {
         if (toast != null) toast!!.cancel()
         super.onPause()
     }
+
+    /*
+    @SuppressLint("ClickableViewAccessibility")
+    fun EditText.setupClearButtonWithAction() {
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(ed: Editable?) {
+                getCurrentCityForecast(ed.toString())
+                val clearIcon = if (ed?.isNotEmpty() == true) R.drawable.ic_clear else 0
+                setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_search_on, 0, clearIcon, 0)
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+        })
+
+        setOnTouchListener(View.OnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= (this.right - this.compoundPaddingRight)) {
+                    this.setText("")
+                    binding.card.isVisible = false
+                    return@OnTouchListener true
+                }
+            }
+            return@OnTouchListener false
+        })
+    }
+
+    private fun initBtn() {
+        binding.btn.setOnClickListener {
+            binding.search.setText("")
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            fetchLocation()
+        }
+    }*/
+
+    /*// Old permission
+   private fun fetchLocation() {
+       val task = fusedLocationProviderClient.lastLocation
+
+       if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+           != PackageManager.PERMISSION_GRANTED && ActivityCompat
+               .checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+       ) {
+           ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+           return
+       }
+       task.addOnSuccessListener {
+           getCurrentCityForecastByCoordinates(it.latitude.toString(),it.longitude.toString())
+       }
+   }
+   // Old permission*/
 }
 
 
