c32e1b32e92dda1f3a945ebdd089f4c814ab4cfb
f.r.malikov <fuad.malikov@rabitabank.com>
2022-01-18 11:53:12 +0400

    Weather app.

--- app/src/main/java/com/example/weatherapptask/data/remote/other/Constants.kt
+++ app/src/main/java/com/example/weatherapptask/data/remote/other/Constants.kt
@@ -4,12 +4,8 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
         const val API_KEY = "545e119b278e9ddd371278175b707bcb"
-        const val BAKU_CITY = "Baku"
         const val UNITS = "metric"
-        const val LAT = "40.3777"
-        const val LON = "49.892"
         const val EXCLUDE = "daily"
-        const val EXCLUDE_CURRENT = "current"
 
         //ROOM Database
         const val DATABASE_NAME = "forecast_database"
